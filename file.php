c55c1e684591959b84a6bd24ff13d9973ac5fdb9
Mihail Georgiev <mihail.georgiev@falcon.io>
2019-03-21 08:56:10 +0200

    JS + some PHP

---
+++ file.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+require "file2.php";
+
+$array = ['duner', 'pizza', 'mousakka'];
+  
+echo json_encode($array);
---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+/**
+* Creates list element to contain all the favourite foods
+* @name createList
+* @return element of list
+*/
+const createList = () => {
+
+	let listElement = document.createElement('ol');
+	document.body.appendChild(listElement);
+	
+	return listElement;
+}
+
+/**
+* Adds li element with given food to the DOM
+* @name addFoodElement
+* @param {String} foodName
+* @param {HtmlElement} listElement
+* @return element of list
+*/
+const addFoodElement = (foodName, listElement) => {
+	let textNode = document.createTextNode(foodName);
+	let liElement = document.createElement('li');
+	liElement.appendChild(textNode);
+	listElement.appendChild(liElement);
+}
+
+const loadHandler = (foods) => {
+
+	const listElement = createList();
+	
+	let foodElements = [];
+	foods.forEach(foodName => {
+		addFoodElement(foodName, listElement);
+	});
+}
+
+window.addEventListener('load', () => {
+	fetch('file.php').then(response => 
+		response.json()
+	).then(foods => {
+		loadHandler(foods);
+	});
+});
